4cf701e51050d53b09b3965c5a3713ea3943993a
Deepak Shettigar <deepaksshettigar@gmail.com>
2026-01-09 01:01:57 +0530

    style: adjust HeroSection and Footer layouts for improved wave background opacity and spacing

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -15,24 +15,24 @@ export function Footer() {
 
             {/* Wave Background Wrapper */}
             <div className="absolute inset-0 overflow-hidden pointer-events-none">
-                <div className="absolute bottom-0 left-0 right-0 z-0 h-64 opacity-70">
+                <div className="absolute bottom-0 left-0 right-0 z-0 h-64">
                     <svg viewBox="0 0 1440 320" className="w-full h-full block align-bottom" preserveAspectRatio="none">
                         {/* Layer 1: Back Wave */}
                         <path
                             fill={BRAND_COLORS.secondary}
-                            fillOpacity="0.4"
+                            fillOpacity="0.6"
                             d="M0,160L48,176C96,192,192,224,288,224C384,224,480,192,576,165.3C672,139,768,117,864,128C960,139,1056,181,1152,197.3C1248,213,1344,203,1392,197.3L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
                         />
                         {/* Layer 2: Middle Wave */}
                         <path
                             fill={BRAND_COLORS.secondary}
-                            fillOpacity="0.6"
+                            fillOpacity="0.8"
                             d="M0,224L48,213.3C96,203,192,181,288,181.3C384,181,480,203,576,213.3C672,224,768,224,864,208C960,192,1056,160,1152,144C1248,128,1344,128,1392,128L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
                         />
                         {/* Layer 3: Front Wave */}
                         <path
                             fill={BRAND_COLORS.secondary}
-                            fillOpacity="0.8"
+                            fillOpacity="1"
                             d="M0,192L48,197.3C96,203,192,213,288,229.3C384,245,480,267,576,250.7C672,235,768,181,864,181.3C960,181,1056,235,1152,234.7C1248,235,1344,181,1392,154.7L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
                         />
                     </svg>
